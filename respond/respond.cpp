56235e6dd2d8c895db110f23780d4fe8fcffedfe
aoumad <abderazzakoumad@gmail.com>
2023-03-30 18:00:11 +0000

    need now to debugg the request and test it

---
+++ respond/respond.cpp
@@ -0,0 +1,65 @@
+#include "respond.h"
+
+/* GET:
+HTTP/1.1 200 OK
+Server: MyServer/1.0
+Content-Type: text/html
+Content-Length: 123
+Connection: close
+
+<html>
+<head>
+    <title>Welcome to my website</title>
+</head>
+<body>
+    <h1>Hello, world!</h1>
+    <p>This is a sample HTML page served by MyServer.</p>
+</body>
+</html>
+*/
+
+/* put:
+HTTP/1.1 200 OK
+Content-Type: text/html
+Content-Length: 56
+Server: MyServer/1.0
+Connection: close
+
+The file has been successfully updated.
+*/
+
+Respond::Respond()
+{
+    _http_method = "";
+    _http_version = "HTTP/1.1";
+    response_body = "";
+}
+
+Respond::~Respond()
+{}
+
+std::string Respond::get_response(std::string http_method, std::string request_path)
+{
+    std::string response;
+
+    if (http_method == "GET")
+        response = handle_get_request(request_path);
+    else if (http_method == "POST")
+        response = handle_post_request(request_path);
+    else if (http_method == "PUT")
+        response = handle_put_request(request_path):
+    else if (http_method == "DELETE")
+        response = handle_delete_request(request_path);
+    else
+    {
+        response = "HTTP/1.1" + " " + status_code(501) + "\r\n";
+        response += "Content-Type: text/html\r\n";
+        response += "Content-Length: " + std::to_string(response_body.length()) + "\r\n";
+        response += "\r\n";
+    }
+
+    headers.erase("Content-Length"); // remove Content-Length header if it exists
+    headers["Content-Length"] = std::to_string(response_body.length()); // add Content-Length header
+
+    
+}
---
+++ respond/respond.hpp
@@ -0,0 +1,32 @@
+#ifndef RESPOND_HPP
+#define RESPOND_HPP
+
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <map>
+
+class Respond
+{
+    private:
+        std::string _http_method;
+        std::string _http_version;
+        std::string _http_path;
+        std::string _response_body;
+        std::map<std::string, std::string> _headers;
+
+        std::string get_file_contents(std::string path);
+        std::string status_code(int code);
+
+        std::string handle_get_request(std::string request_path);
+        std::string handle_post_request(std::string request_path);
+        std::string handle_put_request(std::string request_path);
+        std::string handle_delete_request(std::string request_path);
+    public:
+        Respond();
+        ~Respond();
+        std::string get_response(std::String request_path, std::string http_method);
+};
+
+#endif
